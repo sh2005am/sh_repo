be7d7faebb859228ca9911aa9b47e9ba7adc5102
xenon <shadowonart@gmail.com>
2025-02-27 03:42:41 +0530

    fixed menu bar

--- deepmind/main.js
+++ deepmind/main.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     const header = document.querySelector('.header');
     header.classList.toggle('active'); //  <--  This is the key line!
 }
+let prevScroll = window.pageYOffset;
+window.onscroll = () => {
+  document.querySelector('header').style.top = (prevScroll > window.pageYOffset) ? '0' : '-100px';
+  prevScroll = window.pageYOffset;
+};

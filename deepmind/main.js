42149c449a814773a22aaf4022585f7d294ef0ca
xenon <shadowonart@gmail.com>
2025-02-27 07:57:22 +0530

    Final first page

--- deepmind/main.js
+++ deepmind/main.js
@@ -1,9 +1,22 @@
 function toggleMenu() {
     const header = document.querySelector('.header');
     header.classList.toggle('active'); //  <--  This is the key line!
-}
+};
 let prevScroll = window.pageYOffset;
 window.onscroll = () => {
   document.querySelector('header').style.top = (prevScroll > window.pageYOffset) ? '0' : '-100px';
   prevScroll = window.pageYOffset;
 };
+if (window.innerWidth <= 768) {
+    document.querySelector('.logo').textContent="Deepmind";
+}function updateLogo() {
+    const logo = document.querySelector('.logo');
+    if (window.innerWidth <= 768) {
+      logo.textContent = "Deepmind";
+    } else {
+      logo.textContent = "Google Deepmind";
+    }
+  }
+  window.addEventListener('load', updateLogo);
+  window.addEventListener('resize', updateLogo);
+  
